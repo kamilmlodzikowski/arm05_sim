42daced40f524c7a1af9f7c0fca04044b451d33a
Kamil Młodzikowski <k.mlodzikowski@gmail.com>
2025-11-04 12:02:15 +0100

    Replace debug print statement with logger info in ArucoSpawner for command spawning

--- scripts_arm05/spawn_aruco.py
+++ scripts_arm05/spawn_aruco.py
@@ -51,7 +51,8 @@ class ArucoSpawner(Node):
                 '--pos', f'{x}', f'{y}', '0.0',
                 '--quat', '0.0', '0.0', '0.0', '1.0',
             ]
-            print("Command to spawn aruco:", ' '.join(cmd))  # Debug print statement
+            # print("Command to spawn aruco:", ' '.join(cmd))  # Debug print statement
+            self.get_logger().info("Command to spawn aruco: " + ' '.join(cmd))
 
             result = subprocess.run(cmd, capture_output=True, text=True)
             if result.returncode != 0:
